5e70f74fea9516bf784369f07962f734e939ce88
skarns23 <skarns23@naver.com>
2023-03-10 18:41:30 +0900

    DB 연결

--- src/test/java/com/hart/zuik/TestZuik.java
+++ src/test/java/com/hart/zuik/TestZuik.java
@@ -1,5 +1,24 @@
 package com.hart.zuik;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.hart.mapper.TestMapper;
+
+import lombok.extern.log4j.Log4j2;
+
+@SpringBootTest
+@Log4j2
 public class TestZuik {
+	
+	@Autowired
+	TestMapper mapper;
+	
+	
+	@Test
+	public void Test() {
+		log.info(mapper.getTest());
+	}
 
 }
