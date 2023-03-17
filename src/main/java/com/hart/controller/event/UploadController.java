d8f940eeb1c1740613b94d3322c0a276af498502
DESKTOP-3NQ2B9J\LSG <leesk3732@gmail.com>
2023-03-17 10:33:12 +0900

    레시피 등록 페이지 수정 및 재료검색 기능

--- src/main/java/com/hart/controller/event/UploadController.java
+++ src/main/java/com/hart/controller/event/UploadController.java
@@ -1,14 +1,28 @@
 package com.hart.controller.event;
 
+import java.sql.SQLException;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+
+import com.hart.domain.ProductsVO;
+import com.hart.service.event.EventService;
 
 import lombok.extern.log4j.Log4j2;
 
 @RestController
 @Log4j2
+@RequestMapping("/event/api")
 public class UploadController {
+
+	@Autowired
+	private EventService eventService;
 
 	@PostMapping("/uploadAjax")
 	public void uploadFile(MultipartFile[] uploadFiles) {
@@ -23,4 +37,9 @@ public class UploadController {
 		} // end for
 
 	}// end void
+
+	@GetMapping("/search")
+	public ResponseEntity<List<ProductsVO>> searchProduct(String keyword) throws SQLException {
+		return ResponseEntity.ok(eventService.getList(keyword));
+	}
 }// end class
