271bcbcb34b3d862bdc9fc7c98e6e6b6c723a9b4
DESKTOP-3NQ2B9J\LSG <leesk3732@gmail.com>
2023-03-22 01:17:51 +0900

    나만의 레시피 등록 페이지 완료

--- src/main/java/com/hart/mapper/EventMapper.java
+++ src/main/java/com/hart/mapper/EventMapper.java
@@ -4,12 +4,21 @@ import java.sql.SQLException;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import com.hart.domain.ProductsVO;
+import com.hart.domain.event.CRContentVO;
+import com.hart.domain.event.CRIngredientVO;
+import com.hart.domain.event.CRecipeVO;
 
 @Mapper
 public interface EventMapper {
 
 	List<ProductsVO> getProducts(String keyword) throws SQLException;
+	int selectCrid();
+	//레시피 내용 저장
+	int registerRecipe(@Param("seq")int seq,@Param("recipe")CRecipeVO recipe);
+	int registerContent(@Param("seq")int seq,@Param("content") CRContentVO content);
+	int registerIngredient(@Param("seq")int seq,@Param("ingredient")CRIngredientVO ingredient);
 
 }
--- src/main/java/com/hart/service/event/EventServiceImpl.java
+++ src/main/java/com/hart/service/event/EventServiceImpl.java
@@ -3,9 +3,14 @@ package com.hart.service.event;
 import java.sql.SQLException;
 import java.util.List;
 
+import javax.transaction.Transactional;
+
 import org.springframework.stereotype.Service;
 
 import com.hart.domain.ProductsVO;
+import com.hart.domain.event.CRContentVO;
+import com.hart.domain.event.CRIngredientVO;
+import com.hart.domain.event.CRecipeVO;
 import com.hart.mapper.EventMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
@@ -20,6 +25,21 @@ public class EventServiceImpl implements EventService {
 	@Override
 	public List<ProductsVO> getList(String keyword) throws SQLException {
 		return mapper.getProducts(keyword);
+	}
+
+	@Override
+	@Transactional
+	public void register(CRecipeVO recipe, List<CRContentVO> contents, List<CRIngredientVO> ingredients) {
+		int seq = mapper.selectCrid();
+		
+		mapper.registerRecipe(seq, recipe);
+		for (CRContentVO content : contents) {
+			mapper.registerContent(seq, content);
+		}
+		for (CRIngredientVO ingredient : ingredients) {
+			mapper.registerIngredient(seq, ingredient);
+		}
+
 	}
 
 }
