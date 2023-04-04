72293611f7ed3b5e30d4305c85783231001a065c
junghoon <fpqthel23567@gamil.com>
2023-04-04 18:14:26 +0900

    충돌 수정

--- src/main/java/com/hart/service/ProductsService.java
+++ src/main/java/com/hart/service/ProductsService.java
@@ -20,30 +20,14 @@ public interface ProductsService {
 	public List<CategoryVO> getcategorybar() throws Exception;
 	
 	
-	//public List<CategoryVO> getcategorysmall(String pid) throws Exception;
-	
 	public ProductsDetailVO getProductDetails(String pid) throws Exception;
 
 	
-	//List<CategoryVO> getcategorysmall(int pcno);
-
-	//List<CategoryVO> getcategorysmall(String pcno);
-	
 	public List<ListVO> getproductcatrogrtlist(int pcno) throws Exception;
 
-	
-	//public List<ProductsVO> getproductslistajax(int pcno,int page) throws Exception;
-
-//	public List<ProductlistintegVO> Productlistinteg(int pcno) throws Exception;
-	
-
-	//public List<ListVO> List(ListVO ListVO) throws Exception;
 
 	public List<ListVO> Productlist(ListVO list);
 
-
-
-	//List<ProductsVO> getproductslistajax(int pcno) throws Exception;
 	
 	
 }
