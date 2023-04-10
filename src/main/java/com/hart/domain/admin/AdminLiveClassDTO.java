9f1adf5cc54375f133aa94cf2c506e4942ad7f42
kang <gkatprkd@naver.com>
2023-04-10 14:10:26 +0900

    [ham] 오루 깃 합침

---
+++ src/main/java/com/hart/domain/admin/AdminLiveClassDTO.java
@@ -0,0 +1,24 @@
+package com.hart.domain.admin;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class AdminLiveClassDTO {
+	
+	private String lcid;
+	private String lcday;
+	private String lcstart;
+	private String lcend;
+	private int lcstatus;
+	private String lcteacher;
+	private String lckeyword;
+	private String lcname;
+	private String lcdate;
+	private String lcdayofweek;
+	
+	
+}
