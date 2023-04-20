7d00b381968a49649bbe9324462c5149cb1238b4
skarns23 <skarns23@naver.com>
2023-04-20 11:51:25 +0900

    내용 수정 중

--- src/main/resources/static/js/toastmsg.js
+++ src/main/resources/static/js/toastmsg.js
@@ -1,12 +1,14 @@
 
 function showToast(msg, tag) {
 	$("#toast").attr("style", "visibility: visible;");
+	$("#toast").attr("style", "border-radius: 25px;");
+	$("#toast").attr("style", "background-color: transparent;");
 	$(".toast-content").html(msg);
 	if (tag === 1) {
 		$("#img_icon").attr("src", "/img/check.png");
 	} else {
 		$("#img_icon").attr("src", "/img/error.png");
 	}
-	setTimeout(() => $("#toast").attr("style", "visibility: hidden;"), 2000);
+	setTimeout(() => $("#toast").attr("style", "visibility: hidden;"), 5000);
 	
 	}
